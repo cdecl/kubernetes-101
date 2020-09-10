b39e6c73f41e8c5247e7e7b06450aced5da66232
BK KIM <cdeclare@gmail.com>
2020-09-11 04:54:32 +0900

    update configs

---
+++ metallb/metallb-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://metallb.universe.tf/installation/
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
